7e5861d266573ede268b735debb6f80ca3a7a5f9
zhengfengxiang <1875586562@qq.com>
2020-10-15 14:52:34 +0800

    Initial commit

---
+++ imcp-mq-common/src/main/java/com/amplesky/common/constant/RabbitConstant.java
@@ -0,0 +1,10 @@
+package com.amplesky.common.constant;
+
+public interface RabbitConstant {
+
+    String FANOUT_EXCHANGE_NAME = "fanout-exchange";    //rabbitmq交换机
+    String FANOUT_QUEUE_A = "fanout.A";                 //队列
+    String FANOUT_QUEUE_B = "fanout.B";
+    String FANOUT_QUEUE_C = "fanout.C";
+
+}
