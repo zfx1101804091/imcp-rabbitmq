e0fb5057efd8c24055347625a9dd25d8a998a176
zhengfengxiang <1875586562@qq.com>
2020-10-15 16:07:56 +0800

    修改rabbitmq 默认序列化策略 ,生产者和消费者都得改成一样的策略

---
+++ imcp-mq-consumer/src/main/java/com/amplesky/consumer/listen/DirectReceiver.java
@@ -0,0 +1,23 @@
+package com.amplesky.consumer.listen;
+
+import com.amplesky.common.constant.RabbitConstant;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 消息接收监听类
+ */
+
+@Component
+@RabbitListener(queues = RabbitConstant.DIRECT_QUEUE)//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
